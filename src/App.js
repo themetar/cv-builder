ffa78df7619abee952d60cff1a93830658625101
Dimitar Apostolovski <d.apostolovski@gmail.com>
2020-11-19 21:03:35 +0100

    Add "flat" content

--- src/App.js
+++ src/App.js
@@ -2,10 +2,41 @@ import './App.css';
 import {Component} from "react";
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      cv: {
+        personal: {
+          name: "Your Name",
+          about: "About yourself",
+        },
+        skills: [
+          "Web development",
+          "Brick-laying",
+        ],
+      },
+    };
+  }
+
   render() {
+    const {personal, skills} = this.state.cv;
+
     return (
       <div className="App">
-        
+        <section>
+          <h1>{personal.name}</h1>
+          <p>{personal.about}</p>
+        </section>
+        <section>
+          <h2>Skills</h2>
+          <ul>
+            {skills.map((skill, i) => {
+              return (
+                <li>{skill}</li>
+              );
+            })}
+          </ul>
+        </section>
       </div>
     );
   }
