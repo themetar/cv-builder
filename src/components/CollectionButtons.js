70d33730cf1a1baa00c3e5fb8912b0807d00d9f8
Dimitar Apostolovski <d.apostolovski@gmail.com>
2021-03-18 20:14:49 +0100

    Style CollectionButtons (add and remove)

--- src/components/CollectionButtons.js
+++ src/components/CollectionButtons.js
@@ -1,14 +1,16 @@
+import "./CollectionButtons.css";
+
 function CollectionButtons(props) {
   const {handleAdd, handleRemove, count} = props;
 
   return (
     <div className="CollectionButtons">
-      <button onClick={handleAdd}>
+      <button onClick={handleAdd} className="plus-btn">
         <span aria-hidden="true" className="icon-plus"></span>
         <span className="screen-reader-text">Add</span>
       </button>
       {count > 1 &&
-        <button onClick={handleRemove}>
+        <button onClick={handleRemove} className="cross-btn">
           <span aria-hidden="true" className="icon-cross"></span>
           <span className="screen-reader-text">Remove</span>
         </button>}
