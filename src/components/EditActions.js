f7fcd70640605824119983c70b9a775cd21f5672
Dimitar Apostolovski <d.apostolovski@gmail.com>
2021-04-26 19:59:20 +0200

    Convert EditActions to function component

--- src/components/EditActions.js
+++ src/components/EditActions.js
@@ -1,46 +1,38 @@
-import {Component} from "react";
 import './EditActions.css';
 
-class EditActions extends Component {
-  constructor(props) {
-    super(props);
+function EditActions(props) {
 
-    this.handleClick = this.handleClick.bind(this);
+  function handleClick(event) {
+    props.onAction(event.currentTarget.dataset.action);
   }
 
-  handleClick(event) {
-    this.props.onAction(event.currentTarget.dataset.action);
-  }
+  const {mode} = props;
 
-  render() {
-    const {mode} = this.props;
-
-    return (
-      <div className="EditActions">
-        {mode === "show" &&
-            <button data-action="edit" onClick={this.handleClick} className="edit-button"
-                    title="Edit">
-              <span aria-hidden="true" className="icon-pencil"></span>
-              <span className="screen-reader-text">Edit</span>
-            </button>
-        }
-        {mode !== "show" &&
-            <button data-action="save" onClick={this.handleClick} className="edit-button m-b-1/2 pop-in"
-                    title="Save">
-              <span aria-hidden="true" className="icon-checkmark"></span>
-              <span className="screen-reader-text">Save</span>
-            </button>
-        }
-        {mode !== "show" &&
-            <button data-action="cancel" onClick={this.handleClick} className="edit-button pop-in"
-                    title="Cancel">
-              <span aria-hidden="true" className="icon-undo"></span>
-              <span className="screen-reader-text">Cancel</span>
-            </button>
-        }
-      </div>
-    )
-  }
+  return (
+    <div className="EditActions">
+      {mode === "show" &&
+          <button data-action="edit" onClick={handleClick} className="edit-button"
+                  title="Edit">
+            <span aria-hidden="true" className="icon-pencil"></span>
+            <span className="screen-reader-text">Edit</span>
+          </button>
+      }
+      {mode !== "show" &&
+          <button data-action="save" onClick={handleClick} className="edit-button m-b-1/2 pop-in"
+                  title="Save">
+            <span aria-hidden="true" className="icon-checkmark"></span>
+            <span className="screen-reader-text">Save</span>
+          </button>
+      }
+      {mode !== "show" &&
+          <button data-action="cancel" onClick={handleClick} className="edit-button pop-in"
+                  title="Cancel">
+            <span aria-hidden="true" className="icon-undo"></span>
+            <span className="screen-reader-text">Cancel</span>
+          </button>
+      }
+    </div>
+  );
 }
 
 export default EditActions;
