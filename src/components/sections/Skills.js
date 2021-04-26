a02ce68614fb2e4525907a5ce53026d013b9001d
Dimitar Apostolovski <d.apostolovski@gmail.com>
2021-04-26 21:11:01 +0200

    Convert Skills to function component

--- src/components/sections/Skills.js
+++ src/components/sections/Skills.js
@@ -1,47 +1,40 @@
-import {Component} from "react";
 /*
   Render Skills section data.
   On input, sends data via callback.
 */
-class Skills extends Component {
-  constructor(props) {
-    super(props);
-
-    this.handleChange = this.handleChange.bind(this);
-  }
+function Skills(props) {
 
   /*
     On user input, transform to string array and send to parent via callback
   */
-  handleChange(event) {
+  function handleChange(event) {
     const raw = event.target.value;
     const data = raw.split(",");
-    this.props.onChange(data);
+    props.onChange(data);
   }
 
-  render() {
-    const {mode, data} = this.props;
+  const {mode, data} = props;
 
-    // when displaying
-    const show = (
-      <ul className="skills">
-        {data.map(skill => {
-          return (
-            <li key={skill}>{skill}</li>
-          );
-        })}
-      </ul>
-    );
+  // when displaying
+  const show = (
+    <ul className="skills">
+      {data.map(skill => {
+        return (
+          <li key={skill}>{skill}</li>
+        );
+      })}
+    </ul>
+  );
 
-    // when editing
-    const edit = (
-      <textarea value={data.join(",")} onChange={this.handleChange} className="skills-input" />
-    ); 
+  // when editing
+  const edit = (
+    <textarea value={data.join(",")} onChange={handleChange} className="skills-input" />
+  ); 
 
-    return (
-      mode === "show" ? show : edit
-    );
-  }
+  return (
+    mode === "show" ? show : edit
+  );
+
 }
 
 export default Skills;
